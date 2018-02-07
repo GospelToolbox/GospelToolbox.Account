4b6713f1c48d918c7e6d0f9959426e6b22328042
micahlee <micah@micahlee.com>
2018-02-06 22:36:24 -0500

    correcting production email template url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Account_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://account.gospeltoolbox.org', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://account.gospeltoolbox.org' }
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
