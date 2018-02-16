fb3a737461189c8a89bdb7cc5235ce311071e9b1
micahlee <micah@micahlee.com>
2018-02-15 22:17:59 -0500

    Removing unused devise controller overrides

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { 
     sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'                                                                                         
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   
   namespace :api do
