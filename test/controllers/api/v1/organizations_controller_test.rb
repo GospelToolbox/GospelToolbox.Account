a20501ca4bb4746944e583d1f3edb5e5be083d9d
micahlee <micah@micahlee.com>
2018-02-15 22:04:19 -0500

    Adding more tests around users and organizations

--- test/controllers/api/v1/organizations_controller_test.rb
+++ test/controllers/api/v1/organizations_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
+require 'json'
 
 class Api::V1::OrganizationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "listing organizations for user" do 
+    userA = User.create(email: 'a@test.gospeltoolbox.org', password: 'test!pass', confirmed_at: Time.now)
+    sign_in userA
+    
+    post api_v1_organizations_url, params: { name: 'test org'}, xhr: true
+    assert_response :success
+    org = JSON.parse(@response.body, symbolize_names: true)
+
+    get api_v1_organizations_url, xhr: true
+    assert_response :success
+  end
+
+  test "deleting an organization" do 
+    userA = User.create(email: 'a@test.gospeltoolbox.org', password: 'test!pass', confirmed_at: Time.now)
+    sign_in userA
+    
+    post api_v1_organizations_url, params: { name: 'test org'}, xhr: true
+    assert_response :success
+    org = JSON.parse(@response.body, symbolize_names: true)
+
+    delete api_v1_organization_url(org), xhr: true
+    assert_response :success
+  end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "update user profile" do
+    userA = User.create(email: 'a@test.gospeltoolbox.org', password: 'test!pass', confirmed_at: Time.now)
+    sign_in userA
+    
+    patch api_v1_user_url(userA), params: { first_name: 'newFirst', last_name: 'newLast'}, xhr: true
+    assert_response :success
+
+    updatedUser = User.find userA.id
+    assert_equal 'newFirst', updatedUser.first_name
+    assert_equal 'newLast', updatedUser.last_name
+    
+  end
 end
