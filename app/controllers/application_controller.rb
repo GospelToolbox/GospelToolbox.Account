20d7778394bd067044d339866c0912ce45e7640d
micahlee <micah@micahlee.com>
2018-03-14 21:53:06 -0400

    Adding version info and timestamp to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   #include Pundit
   protect_from_forgery with: :exception
-
   before_action :authenticate_user!
+  helper_method :app_version
+
+  protected
+
+  def app_version
+    @app_version ||= `git show --pretty=%H`[0...8]
+  end
 end
