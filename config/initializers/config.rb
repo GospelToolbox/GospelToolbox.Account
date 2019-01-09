96ad3d764a8548f11c943c2e59f58bb5147ad44b
Micah Lee <micah@micahlee.com>
2019-01-08 20:44:28 -0500

    Update dependencies

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,12 +1,13 @@
 #App sub uri for rails app
 if ENV['APP_SUBURI'].present?
-  APP_SUB_URi = ENV['APP_SUBURI']
+  APP_SUB_URI = ENV['APP_SUBURI']
   SITE_URL = ENV['SITE_HOST'] + ENV['APP_SUBURI']
 elsif ENV['SCRIPT_NAME'].present?
-  APP_SUB_URi = ENV['SCRIPT_NAME']
+  APP_SUB_URI = ENV['SCRIPT_NAME']
   SITE_URL = ENV['SITE_HOST'] + ENV['SCRIPT_NAME']
 end
+
 #if Puma Server
 if ENV['PASSENGER'].blank?
-  Account::Application.config.relative_url_root = APP_SUB_URi
+  Account::Application.config.relative_url_root = APP_SUB_URI
 end
